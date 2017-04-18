8dd799285e86c74266a4f744512d1301127284bb
Madison <19233698150@163.com>
2017-04-19 00:23:48 +0800

    use bower

--- gui/Gruntfile.js
+++ gui/Gruntfile.js
@@ -5,14 +5,30 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
             options: {
-                jshintrc:'.jshintrc'
+                jshintrc: '.jshintrc'
             },
-            build: ['Gruntfile.js','gui.js','script/*/*.js']
+            build: ['Gruntfile.js', 'gui.js', 'script/*/*.js']
+        },
+        "bower": {
+            "install": {
+                options: {
+                    copy: false,
+                    targetDir: 'component',
+                    layout: 'byType',
+                    install: true,
+                    verbose: false,
+                    prune: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bower-task');
 
     // Default task
     grunt.registerTask('default', ['jshint']);
