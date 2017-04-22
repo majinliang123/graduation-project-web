d3e8a71d43cdc245afe32f47f10530335b43c675
Madison <19233698150@163.com>
2017-04-22 19:24:11 +0800

    add less

--- gui/Gruntfile.js
+++ gui/Gruntfile.js
@@ -1,35 +1,59 @@
 'use strict';
 
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            build: ['Gruntfile.js', 'gui.js', 'script/*/*.js']
-        },
-        'bower': {
-            'install': {
-                options: {
-                    copy: false,
-                    targetDir: 'component',
-                    layout: 'byComponent',
-                    install: true,
-                    verbose: false,
-                    prune: false,
-                    cleanTargetDir: false,
-                    cleanBowerDir: false,
-                    bowerOptions: {}
-                }
-            }
-        }
-    });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			build: ['Gruntfile.js', 'gui.js', 'script/*/*.js']
+		},
+		'bower': {
+			'install': {
+				options: {
+					copy: false,
+					targetDir: 'component',
+					layout: 'byComponent',
+					install: true,
+					verbose: false,
+					prune: false,
+					cleanTargetDir: false,
+					cleanBowerDir: false,
+					bowerOptions: {}
+				}
+			}
+		},
+		less: {
+			compile: {
+				files: {
+					'style/dist.css': 'less/*.less'
+				}
+			}
+		},
 
-    // Load the plugin that provides the 'uglify' task.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-bower-task');
+		//css压缩插件
+		cssmin: {
+			options: {
+				keepSpecialComments: 0
+			},
+			compress: {
+				files: {
+					'style/dist.min.css': [
+						'style/dist.css'
+					]
+				}
+			}
+		}
 
-    // Default task
-    grunt.registerTask('default', ['jshint']);
+	});
+
+	// Load the plugin that provides the 'uglify' task.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-bower-task');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+	// Default task
+	grunt.registerTask('default', ['jshint']);
 };
