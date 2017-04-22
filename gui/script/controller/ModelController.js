420e3b94251c92217ef58a0f4e8cf5e7e6a2fcd2
Madison <19233698150@163.com>
2017-04-23 00:55:06 +0800

    format code for build

--- gui/script/controller/ModelController.js
+++ gui/script/controller/ModelController.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 angular.module('coolest')
 	.controller('ModelController', ['Model','$scope', '$rootScope', function (Model,$scope, $rootScope) {
 		$scope.detail = {};
 		$scope.model = {};
-		$rootScope.$watch('detail', function(newValue){
+		$rootScope.$watch('detail', function(){
 			$scope.detail = $rootScope.detail;
 			$scope.model = Model[$rootScope.path];
 		});
