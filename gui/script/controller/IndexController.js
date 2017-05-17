77a339ded10c6abeec8b6fc3f689610537abe341
Madison <18233698150@163.com>
2017-05-17 23:27:42 +0800

    add authority

--- gui/script/controller/IndexController.js
+++ gui/script/controller/IndexController.js
@@ -1,24 +1,30 @@
 'use strict';
 
-angular.module('coolest', ['ngCookies'], function ($locationProvider) {
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
-})
-	.controller('IndexController', ['$location', '$scope', '$cookies', '$rootScope', function ($location, $scope, $cookies, $rootScope) {
+angular.module('coolest', ['ngCookies'], function($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    })
+    .controller('IndexController', ['$location', '$scope', '$cookies', '$rootScope', 'Search', function($location, $scope, $cookies, $rootScope, Search) {
 
-		$scope.main = './view/welcome.html';
-		$scope.nav = './view/nav.html';
+        $scope.main = './view/welcome.html';
+        $scope.nav = './view/nav.html';
 
-		$rootScope.username = $cookies.get('username');
+        $rootScope.username = $cookies.get('username');
+        fetchAuthority();
+        $scope.$on('$locationChangeStart', function() {
+            var url = $location.url();
+            var urlArray = url.split('/');
+            var path = urlArray[2];
+            if (path) {
+                $scope.main = './view/main.html';
+            }
+        });
 
-		$scope.$on('$locationChangeStart', function () {
-			var url = $location.url();
-			var urlArray = url.split('/');
-			var path = urlArray[2];
-			if (path) {
-				$scope.main = './view/main.html';
-			}
-		});
-	}]);
+        function fetchAuthority() {
+            Search.get('/api/authority/', 'madison', '').then(function(data) {
+                $rootScope.authority = data;
+            });
+        }
+    }]);
--- gui/script/controller/SidebarController.js
+++ gui/script/controller/SidebarController.js
@@ -1,15 +1,16 @@
 'use strict';
 
 angular.module('coolest')
-    .controller('SidebarController', ['SearchForms', '$scope', function (SearchForms, $scope) {
+    .controller('SidebarController', ['SearchForms', '$scope', function(SearchForms, $scope) {
         var keys = Object.keys(SearchForms);
         $scope.sidebars = [];
-        keys.forEach(function (element) {
+        keys.forEach(function(element) {
             var sidebar = {
-                name: element.toLowerCase().replace(/\b[a-z]/g, function (letter) {
+                name: element,
+                displayName: element.toLowerCase().replace(/\b[a-z]/g, function(letter) {
                     return letter.toUpperCase();
                 }),
-                link: './' + element.toLowerCase()
+                link: '/gui/' + element.toLowerCase()
             };
             $scope.sidebars.push(sidebar);
         });
